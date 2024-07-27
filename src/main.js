220b4081f7e0b57db547f2bcb4a6531742479a32
July-Month <1345464419@qq.com>
2024-07-27 16:18:51 +0800

    home lazyPlugin optimize

--- src/main.js
+++ src/main.js
@@ -6,26 +6,12 @@ import pinia from '@/stores'
 
 import App from './App.vue'
 import router from './router'
-import { useIntersectionObserver } from '@vueuse/core'
+// 引入懒加载指令插件并且注册
+import { lazyPlugin } from '@/directives'
 
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
-
-// 定义全局指令
-
-app.directive('img-lazy', {
-  mounted(el, binding) {
-    // el: 指令绑定的那个元素
-    // binding: binding.value  指令 = 号 后面绑定的表达式的值
-    useIntersectionObserver(el, ([{ isIntersecting }]) => {
-      if (isIntersecting) {
-        // 图片进入视口区域
-        el.src = binding.value
-      }
-    })
-  }
-})
